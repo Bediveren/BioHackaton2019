e269565c0687c1bae6ea7fb83af6bc1cdc25ad10
Renican <inkomirikan@gmail.comm>
2019-08-04 10:42:26 +0300

    Returns count for cells and sets an image at /get_image endpoint

--- flask/main.py
+++ flask/main.py
@@ -2,26 +2,30 @@ from flask import Flask, redirect, url_for, request, jsonify
 from flask_cors import CORS
 import cv2
 import io
-from PIL import Image
+from recog import count_cells
+from flask import send_file
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+@app.route('/get_image')
+def get_image():
+    return send_file("result.jpeg", mimetype='image/gif')
 
 @app.route('/upload',methods = ['POST', 'GET'])
 def upload():
     if request.method == 'GET':
         return jsonify({'GET':'SUCCESS'})
     if request.method == 'POST':
+        imagefile = request.files.get('file', '')
         print('Recieved POST request')
-
+        from PIL import Image
         image = request.files["file"]
         image_bytes = Image.open(io.BytesIO(image.read()))
-        image_bytes.save('hello.jpg')
-        return jsonify({'count':'420'})
+        image_bytes.save('ImageRecieved.jpg')
+        cell_count = count_cells()
+        print(cell_count)
+        return jsonify({'count': str(cell_count)})
 
 if __name__ == '__main__':
    app.run(debug = True, port = 3000)
---
+++ flask/recog.py
@@ -0,0 +1,29 @@
+from skimage import io, filters
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+from skimage.exposure import histogram
+from skimage import measure, transform
+from skimage.transform import rescale, resize, downscale_local_mean
+from PIL import Image
+
+
+def count_cells():
+    #Preparing data
+    im = io.imread('ImageRecieved.jpg', as_grey=True)
+    #image_rescaled = rescale(im, 2.5, anti_aliasing=True)
+    #io.imsave('../data/ScaleRotate15gimg1.jpg', image_rotated)
+    #im = np.asarray( image, dtype="int32" )
+    val = filters.threshold_otsu(im)
+    image_filtered = ndimage.binary_fill_holes(im < val)
+    image_eroded = ndimage.binary_erosion(image_filtered, structure=np.ones((2,2)))
+
+    #Remove irregular areas
+    cellcount = measure.label(image_filtered)
+    #print('CELL COUNT' + str(cellcount.max()))
+    #io.imsave('result.jpg', image_filtered)
+
+    im = Image.fromarray(image_filtered)
+    im.save("result.jpeg")
+    return cellcount.max()
